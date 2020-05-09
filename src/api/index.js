dcf7f02792bb50afd275affe7fe0375ffec9b14a
James-Nu <59280634+James-Nu@users.noreply.github.com>
2020-05-09 15:36:04 +0300

    Refactored

--- src/api/index.js
+++ src/api/index.js
@@ -41,31 +41,3 @@ export const fetchCountries = async () => {
 
 
 
-// import axios from 'axios';
-
-// const url = "https://covid19.mathdro.id/api";
-
-// export const fetchData = async () => {
-//     try{
-//         const { data:{confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
- 
-//         return { confirmed, recovered, deaths,lastUpdate };
-//     }catch(error){
-
-//     }
-// }
-
-// export const fetchDailyData = async () => {
-//     try{
-//         const { data } = await axios.get(`${url}/daily`);
-//         const modifiedData = data.map((dailyData)=> ({
-//             confirmed: dailyData.confirmed.total,
-//             deaths: dailyData.deaths.total,
-//             data: dailyData.reportDate,
-//         }));
-
-//         return modifiedData;
-//     }catch(error){
-
-//     }
-// }
